df61ec6f6e83fe6f66204c6551944e1fada2e69c
Helena Standaert <helena.standaert@student.arteveldehs.be>
2014-06-06 21:26:17 +0200

    nav update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,51 +1,53 @@
 'use strict';
 
 var app = angular
-  .module('oggiNgApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'cn.offCanvas'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/app', {
-        templateUrl: 'views/preload.html',
-        controller: 'AppCtrl',
-        resolve: {
-          appInitialized: appCtrl.initialize
-        }
-      })
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-       .when('/month', {
-        templateUrl: 'views/month.html',
-        controller: 'MonthCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-  .run(['$rootScope', '$timeout', '$location', '$window', '$http',function($rootScope, $timeout, $location, $window, $http){
-        $rootScope.appInitialized = false;
+    .module('oggiNgApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'cn.offCanvas'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/app', {
+                templateUrl: 'views/preload.html',
+                controller: 'AppCtrl',
+                resolve: {
+                    appInitialized: appCtrl.initialize
+                }
+            })
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/month', {
+                templateUrl: 'views/month.html',
+                controller: 'MonthCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .run(['$rootScope', '$timeout', '$location', '$window', '$http',
+        function($rootScope, $timeout, $location, $window, $http) {
+            $rootScope.appInitialized = false;
 
-        $rootScope.$on('$routeChangeStart', function(event, next, current){
-           if(!$rootScope.appInitialized){
-               $location.path('/app');
-           }else if($rootScope.appInitialized && $location.path() === '/app'){
-               $location.path('/');
-           }
-        })
+            $rootScope.$on('$routeChangeStart', function(event, next, current) {
+                if (!$rootScope.appInitialized) {
+                    $location.path('/app');
+                } else if ($rootScope.appInitialized && $location.path() === '/app') {
+                    $location.path('/');
+                }
+            })
 
-        $rootScope.goBack = function(){
-            setTimeout(function() {
-              $window.history.back();
-            },100);
+            $rootScope.goBack = function() {
+                setTimeout(function() {
+                    $window.history.back();
+                }, 100);
+            }
         }
-    }]);
+    ]);
 
 /*
     AppCtrl
@@ -56,37 +58,35 @@ var app = angular
     * Return the promises
     * Resolve for each route
 */
-var appCtrl = app.controller('AppCtrl', ['$scope', '$location', 'appInitialized', '$http', '$rootScope', 'offCanvas', function($scope, $location, appInitialized, $http, $rootScope){
-    if(appInitialized){
-        $location.path('/');
+var appCtrl = app.controller('AppCtrl', ['$scope', '$location', 'appInitialized', '$http', '$rootScope', 'offCanvas',
+    function($scope, $location, appInitialized, $http, $rootScope) {
+        if (appInitialized) {
+            $location.path('/');
+        }
     }
-}]);
+]);
 
-appCtrl.initialize = ['$rootScope', '$q', '$timeout', function($rootScope, $q, $timeout){
-    var deferred = $q.defer();
-
-    $rootScope.toggleNav = function(){
-      $.sidr('toggle', 'offcanvas-navigation');
-    };
-
-    $rootScope.shout = function(){
-      console.log('AY');
-    };
+appCtrl.initialize = ['$rootScope', '$q', '$timeout',
+    function($rootScope, $q, $timeout) {
+        var deferred = $q.defer();
 
 
-    $rootScope.appInitialized = true;
-    deferred.resolve(true);
+        $rootScope.appInitialized = true;
+        deferred.resolve(true);
 
-    return deferred.promise;
-}];
+        return deferred.promise;
+    }
+];
 
-var navCtrl = app.controller('navCtrl', ['offCanvas', '$rootScope', function(offCanvas, $rootScope) {
-      $rootScope.toggle = offCanvas.toggle;
-    }])
+var navCtrl = app.controller('navCtrl', ['offCanvas', '$rootScope',
+    function(offCanvas, $rootScope) {
+        $rootScope.toggleNav = offCanvas.toggle;
+    }
+])
     .factory('offCanvas', function(cnOffCanvas) {
-      return cnOffCanvas({
-        controller: 'navCtrl',
-        controllerAs: 'nav',
-        templateUrl: 'views/partials/offcanvas.html'
-      })
+        return cnOffCanvas({
+            controller: 'navCtrl',
+            controllerAs: 'nav',
+            templateUrl: 'views/partials/offcanvas.html'
+        })
     });
