48f7b6763c73a3a0f06230c45207c7b32d0f19af
Helena Standaert <helena.standaert@gmail.com>
2014-06-08 19:27:11 +0200

    added kalenderkes

--- app/scripts/controllers/day.js
+++ app/scripts/controllers/day.js
@@ -8,5 +8,64 @@ angular.module('oggiApp.controllers')
                 'AngularJS',
                 'Karma'
             ];
+
+            $scope.months = [
+                'Januari',
+                'Februari',
+                'Maart',
+                'April',
+                'Mei',
+                'Juni',
+                'Juli',
+                'Augustus',
+                'September',
+                'Oktober',
+                'November',
+                'December'
+            ];
+
+            $scope.daysShort = [
+                'Zo',
+                'MA',
+                'DI',
+                'WO',
+                'DO',
+                'VR',
+                'ZA'
+            ];
+
+            $scope.today = new Date();
+            $scope.currentDay = $scope.today.getDate();
+            $scope.startYear = $scope.today.getMonth() > 8 ? $scope.today.getFullYear() : $scope.today.getFullYear() - 1;
+
+            $scope.previousDay = function(){
+                $scope.today.setDate($scope.today.getDate() - 1);
+
+                if($scope.today.getMonth() == 7 && $scope.today.getDate() == 31) {
+                    $scope.today.setYear($scope.startYear + 1);
+                }
+
+                $scope.setDayString();
+            };
+
+            $scope.nextDay = function(){
+                $scope.today.setDate($scope.today.getDate() + 1);
+
+                if($scope.today.getMonth() == 8 && $scope.today.getDate() == 1) {
+                    $scope.today.setYear($scope.startYear);
+                }
+
+                $scope.setDayString();
+            };
+
+            $scope.setDayString = function(){
+                var result = '';
+                result += $scope.daysShort[$scope.today.getDay()];
+                result += ' ' + $scope.today.getDate();
+                result += ' ' + $scope.months[$scope.today.getMonth()];
+                result += ' ' + $scope.today.getFullYear();
+
+                $scope.currentDay = result;
+            }
         }
     ]);
--- app/scripts/controllers/week.js
+++ app/scripts/controllers/week.js
@@ -9,6 +9,21 @@ angular.module('oggiApp.controllers')
                 'Karma'
             ];
 
+            $scope.monthsShort = [
+                'Jan.',
+                'Feb.',
+                'Mrt.',
+                'Apr.',
+                'Mei.',
+                'Jun.',
+                'Jul.',
+                'Aug.',
+                'Sep.',
+                'Okt.',
+                'Nov.',
+                'Dec.'
+            ];
+
             Date.prototype.getWeekNumber = function(){
                 var d = new Date(+this);
                 d.setHours(0,0,0);
@@ -17,15 +32,77 @@ angular.module('oggiApp.controllers')
             };
 
             $scope.today = new Date();
-            $scope.fWeekDay = $scope.today.setDate($scope.today.getDate() + 4 - ($scope.today.getDay() || 7));
-            $scope.currentWeek = $scope.fWeekDay.getWeekNumber();
+            $scope.currentWeek = $scope.today.getWeekNumber();
+            $scope.currentYear = $scope.today.getFullYear();
+            $scope.startYear = $scope.today.getMonth() > 8 ? $scope.currentYear : $scope.currentYear - 1;
 
             $scope.previousWeek = function(){
-                console.log($scope.fWeekDay);
+                $scope.currentMonday.setDate($scope.currentMonday.getDate() - 7);
+                $scope.currentWeek = $scope.currentMonday.getWeekNumber();
+
+                switch($scope.currentMonday.getMonth()){
+                    case 0:
+                        $scope.currentYear = $scope.startYear;
+                        break;
+                    case 8:
+                        $scope.currentYear = $scope.startYear + 1;
+                        break;
+                }
+                $scope.currentMonday.setYear($scope.currentYear);
+                $scope.setCurrentMonday($scope.currentMonday);
+
+                $scope.buildWeekCalendar();
             };
 
             $scope.nextWeek = function(){
+                $scope.currentMonday.setDate($scope.currentMonday.getDate() + 7);
+                $scope.currentWeek = $scope.currentMonday.getWeekNumber();
 
+                switch($scope.currentMonday.getMonth()){
+                    case 0:
+                        $scope.currentYear = $scope.startYear + 1;
+                        break;
+                    case 8:
+                        $scope.currentYear = $scope.startYear;
+                        break;
+                }
+                $scope.currentMonday.setYear($scope.currentYear);
+                $scope.setCurrentMonday($scope.currentMonday);
+
+                $scope.buildWeekCalendar();
             };
+
+            $scope.buildWeekCalendar = function(){
+                $scope.daysInWeek = [];
+
+                var curr_day = $scope.currentMonday.getDate();
+                var curr_month = $scope.currentMonday.getMonth();
+                var nDays = new Date($scope.currentMonday.getFullYear(), $scope.currentMonday.getMonth() + 1, 0).getDate();
+
+                for(var i = 0; i < 7; i++){
+                    var dtd;
+                    var mtd;
+
+                    if(curr_day + i > nDays){
+                        dtd = curr_day = 1;
+                        mtd = curr_month + 1 > 11 ? 0 : curr_month + 1;
+                        curr_month = mtd;
+                    }
+                    else {
+                        dtd = curr_day + i;
+                        mtd = curr_month;
+                    }
+
+                    $scope.daysInWeek.push({day: dtd, month: $scope.monthsShort[mtd]});
+                }
+            };
+
+            $scope.setCurrentMonday = function(cd){
+                var d = typeof(cd) === 'undefined' ? new Date() : cd;
+                d.setDate(d.getDate() - (d.getDay() + 6) % 7);
+                $scope.currentMonday = d;
+            }
+
+            $scope.setMonday = function(){}
         }
     ]);
