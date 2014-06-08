9d46dd8dd6f926d8f0b0a7409f6d852c3a2f2a87
nancy vanobbergen <onbekende_1990@msn.com>
2014-06-08 16:08:04 +0200

    nj

--- app/scripts/controllers/month.js
+++ app/scripts/controllers/month.js
@@ -8,5 +8,105 @@ angular.module('oggiNgApp')
                 'AngularJS',
                 'Karma'
             ];
+
+            $scope.months = [
+                'Januari',
+                'Februari',
+                'Maart',
+                'April',
+                'Mei',
+                'Juni',
+                'Juli',
+                'Augustus',
+                'September',
+                'Oktober',
+                'November',
+                'December'
+            ];
+
+            $scope.daysShort = [
+                'MA',
+                'DI',
+                'WO',
+                'DO',
+                'VR',
+                'ZA',
+                'ZO'
+            ];
+
+            $scope.curr_date = new Date();
+            $scope.original_date = new Date();
+            $scope.currentMonth = $scope.curr_date.getMonth();
+            $scope.currentMonthFull =  $scope.months[$scope.currentMonth];
+            $scope.currentYear = $scope.curr_date.getFullYear();
+
+            $scope.previousMonth = function(){
+                $scope.currentMonth = $scope.currentMonth != 0 ?  $scope.currentMonth - 1 : 11;
+                $scope.curr_date.setMonth($scope.currentMonth);
+                $scope.currentMonthFull =  $scope.months[$scope.currentMonth];
+
+                switch($scope.currentMonth){
+                    case 11:
+                        $scope.currentYear = $scope.currentYear - 1;
+                        break;
+                    case 7:
+                        $scope.currentYear = $scope.currentYear + 1;
+                        break;
+                }
+                $scope.curr_date.setYear($scope.currentYear);
+                $scope.createCalendar();
+            };
+
+            $scope.nextMonth = function(){
+                $scope.currentMonth = $scope.currentMonth != 11 ?  $scope.currentMonth + 1 : 0;
+                $scope.curr_date.setMonth($scope.currentMonth)
+                $scope.currentMonthFull =  $scope.months[$scope.currentMonth];
+
+                switch($scope.currentMonth){
+                    case 0:
+                        $scope.currentYear = $scope.currentYear + 1;
+                        break;
+                    case 8:
+                        $scope.currentYear = $scope.currentYear - 1;
+                        break;
+                }
+                $scope.curr_date.setYear($scope.currentYear);
+                $scope.createCalendar();
+            };
+
+            $scope.createCalendar = function(){
+                $scope.daysInMonth = [];
+
+                var p_month = $scope.currentMonth == 0 ? 11 : $scope.currentMonth - 1;
+
+                var fDay = new Date($scope.currentYear, $scope.currentMonth, 1);
+                var lDay = new Date($scope.currentYear, $scope.currentMonth + 1, 0);
+                var ndipm = new Date($scope.currentYear, p_month + 1, 0).getDate();
+
+                var start = fDay.getDay() != 0 ? fDay.getDay() - 1 : 6;
+
+                var dim = lDay.getDate();
+                var dtd = Math.ceil((dim+start)/7) * 7;
+
+                for(var q = 0; q <= 6; q++) {
+                    $scope.daysInMonth.push({date: $scope.daysShort[q], class: 'header'});
+                }
+
+                for(var i = ndipm - start + 1; i <= ndipm; i++) {
+                    $scope.daysInMonth.push({date: i, class: 'not-in-month'});
+                }
+
+                for(var a = 1; a <= dim; a++) {
+                    var cls =   a == $scope.original_date.getDate() &&
+                                $scope.original_date.getMonth() == $scope.currentMonth ?
+                                'today' : 'in-month';
+                    console.log($scope.original_date.getDate());
+                    $scope.daysInMonth.push({date: a, class: cls});
+                }
+
+                for(var x = 1; x <= dtd - start - dim; x++) {
+                    $scope.daysInMonth.push({date: x, class: 'not-in-month'});
+                }
+            }
         }
     ]);
--- app/scripts/controllers/week.js
+++ app/scripts/controllers/week.js
@@ -8,5 +8,24 @@ angular.module('oggiNgApp')
                 'AngularJS',
                 'Karma'
             ];
+
+            Date.prototype.getWeekNumber = function(){
+                var d = new Date(+this);
+                d.setHours(0,0,0);
+                d.setDate(d.getDate()+4-(d.getDay()||7));
+                return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+            };
+
+            $scope.today = new Date();
+            $scope.fWeekDay = $scope.today.setDate($scope.today.getDate() + 4 - ($scope.today.getDay() || 7));
+            $scope.currentWeek = $scope.fWeekDay.getWeekNumber();
+
+            $scope.previousWeek = function(){
+                console.log($scope.fWeekDay);
+            };
+
+            $scope.nextWeek = function(){
+
+            };
         }
     ]);
