1655e6536a3e8877a82053f2fa735a1b7931d1d1
Helena Standaert <helena.standaert@gmail.com>
2014-06-12 14:22:10 +0200

    Schedule added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('oggiApp.controllers')
-    .controller('oggiApp.controllers.MainCtrl', ['$scope', 'oggiApp.services.WeatherSrvc',
-        function($scope, WeatherSrvc) {
+    .controller('oggiApp.controllers.MainCtrl', ['$scope', 'oggiApp.services.WeatherSrvc', '$rootScope',
+        function($scope, WeatherSrvc, $rootScope) {
             $scope.weather;
 
             $scope.awesomeThings = [
@@ -11,12 +11,32 @@ angular.module('oggiApp.controllers')
                 'Karma'
             ];
 
+            $scope.coursesToday = [];
+
             $scope.init_informations = function(){
                 //$scope.weather = WeatherSrvc.getCurrentWeather();
             };
 
-            $scope.buildTodaysSchedule = function(){
-                
-            }
+            $scope.createDailySchedule = function(){
+                var date = new Date();
+                var day = date.getDay() + 1;
+
+                var courseDay = $.grep($rootScope.user.klas.schedules, function(a){
+                    return a.day.id == day;
+                });
+
+                courseDay.sort(function (a, b) {
+                    return new Date(a.timeslot.start) - new Date(b.timeslot.start);
+                });
+
+                $.each(courseDay, function(k, v){
+                    var name = v.course.name;
+                    var start = new Date(v.timeslot.start);
+                    var time = ('0' + start.getHours()).slice(-2) + ':' + ('0' + start.getMinutes()).slice(-2);
+                    var classroom = 'B1';
+
+                    $scope.coursesToday.push({name: name, start: time, classroom: classroom});
+                });
+            };
         }
     ]);
--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -1,10 +1,68 @@
 'use strict';
 
 angular.module('oggiApp.controllers')
-  .controller('oggiApp.controllers.ScheduleCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('oggiApp.controllers.ScheduleCtrl', ['$scope', '$rootScope',
+        function ($scope, $rootScope) {
+            $scope.awesomeThings = [
+              'HTML5 Boilerplate',
+              'AngularJS',
+              'Karma'
+            ];
+
+            $scope.courseDays = [
+                'Maandag',
+                'Dinsdag',
+                'Woensdag',
+                "Donderdag",
+                'Vrijdag'
+            ];
+
+            $scope.courses = [];
+            $scope.dayIndex = 0;
+            $scope.dayName = '';
+
+            $scope.setCurrentDay = function(){
+                var date = new Date();
+                var day = date.getDay() - 1;
+
+                $scope.dayIndex = day;
+                $scope.dayName = $scope.courseDays[day];
+            };
+
+            $scope.previousDay = function(){
+                $scope.dayIndex = $scope.dayIndex != 0 ? $scope.dayIndex - 1 : 4;
+                $scope.dayName = $scope.courseDays[$scope.dayIndex];
+
+                $scope.createSchedule();
+            };
+
+            $scope.nextDay = function(){
+                $scope.dayIndex = $scope.dayIndex != 4 ? $scope.dayIndex + 1 : 0;
+                $scope.dayName = $scope.courseDays[$scope.dayIndex];
+console.log($scope.dayIndex);
+                $scope.createSchedule();
+            };
+
+            $scope.createSchedule = function(){
+                $scope.courses = [];
+
+                var courseDay = $.grep($rootScope.user.klas.schedules, function(a){
+                    return a.day.id == $scope.dayIndex + 1;
+                });
+
+                courseDay.sort(function (a, b) {
+                    return new Date(a.timeslot.start) - new Date(b.timeslot.start);
+                });
+
+                $.each(courseDay, function(k, v){
+                    var name = v.course.name;
+                    var start = new Date(v.timeslot.start);
+                    var time = ('0' + start.getHours()).slice(-2) + ':' + ('0' + start.getMinutes()).slice(-2);
+                    var classroom = 'B1';
+
+                    $scope.courses.push({name: name, start: time, classroom: classroom});
+                });
+            };
+        }
+    ]
+);
